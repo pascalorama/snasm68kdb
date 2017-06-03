44c1fd33eb6c9ce3ef19ac9a4b7b6d707a7f779b
Bastien Nocera <hadess@hadess.net>
2017-06-03 11:01:49 +0200

    build: Support running configure with srcdir != builddir

--- autogen.sh
+++ autogen.sh
@@ -3,12 +3,21 @@
 echo "Generating build information using aclocal, autoheader, automake and autoconf"
 echo "This may take a while ..."
 
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
+
+olddir=$(pwd)
+
+cd $srcdir
+
 # Regenerate configuration files.
 
 aclocal
 autoheader
 automake --include-deps --add-missing --copy 
 autoconf
+
+cd "$olddir"
 
 echo "Now you are ready to run ./configure."
 echo "You can also run  ./configure --help for extra features to enable/disable."
